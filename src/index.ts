8d2bdcfbb34b7676172a39511f40e14f25c6449a
Richard Hyesung O <orichh@gmail.com>
2022-01-20 19:56:28 -0500

    Correct spend call response

--- src/index.ts
+++ src/index.ts
@@ -129,15 +129,17 @@ const getBalanceDifferences = (
   payerBalanceChanges: { [key: string]: number },
   payerBalances: { [key: string]: number }
 ) => {
+  let arr = [];
   for (const [payer, points] of Object.entries(payerBalances)) {
     if (payerBalanceChanges[payer] === points) {
       delete payerBalanceChanges[payer];
     } else {
+      arr.push({ payer: payer, points: points - payerBalanceChanges[payer] });
       payerBalanceChanges[payer] = points - payerBalanceChanges[payer];
     }
   }
 
-  return payerBalanceChanges;
+  return arr;
 };
 
 ////////////////////
